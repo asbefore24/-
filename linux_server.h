b3943d15bbb89865f66abf8315f50603ac8da4b3
Czh2 <1149150961@qq.com>
2026-05-31 16:36:48 +0800

    server

--- linux_server.h
+++ linux_server.h
@@ -1,19 +1,61 @@
 #include <iostream>
-#include <string>
-#include <cstring>
-#ifndef _WIN32
 #include <sys/socket.h>
-#endif
+#include <unistd.h>
+#include <netinet/in.h>
 
 class Server {
 private:
+    int server_fd = 0;
+    int port = 8080;
+    struct sockaddr_in address;
 
 public:
-    Server() {
-        int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (server_fd == -1) {
+    explicit Server(int p) {
+        port = p;
+        server_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_fd < 0) {
             throw std::runtime_error("Socket creation failed");
         }
+        int opt = 1;
+        setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(port);
+
+        if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+            throw std::runtime_error("Bind failed");
+        }
+
+        if (listen(server_fd, 5) < 0) {
+            throw std::runtime_error("Listen failed");
+        }
+
+        std::cout << "Server listening on port " << port << std::endl;
     };
     
+    ~Server() {
+        if (server_fd >= 0) {
+            close(server_fd);
+        }
+    }
+
+    void run() {
+        while (true) {
+            std::cout << "Waiting for connection..." << std::endl;
+            socklen_t addrlen = sizeof(address);
+            int client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
+            if (client_fd < 0) {
+                std::cerr << "Accept failed" << std::endl;
+                continue;
+            }
+            std::cout << "Client connected";
+            // handle
+            
+
+            std::cout << "handle complete";
+            close(client_fd);
+            std::cout << "Client disconnected" << std::endl;
+        }
+    }
 };
